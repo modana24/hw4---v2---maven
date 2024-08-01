7bb52e92e98a78baf9dce2c48d126cb298b133db
hime <dasha.motry2002@mail.ru>
2024-08-01 10:36:06 +0300

    начало

---
+++ src/main/java/com/hw4v2/skyprohw4v2/Skyprohw4v2Application.java
@@ -0,0 +1,13 @@
+package com.hw4v2.skyprohw4v2;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Skyprohw4v2Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Skyprohw4v2Application.class, args);
+	}
+
+}
